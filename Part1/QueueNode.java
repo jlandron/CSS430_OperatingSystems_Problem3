e6d2f8a9e64e51fe759dd03aa1f76d3d3201b2bf
Joshua Landron <45753071+jlandron@users.noreply.github.com>
2019-11-03 20:46:22 -0800

    .

--- Part1/QueueNode.java
+++ Part1/QueueNode.java
@@ -3,16 +3,16 @@ import java.util.Vector;
 class QueueNode {
     private Vector<Integer> tidVector; // using Integer object instead of int
 
-    
-    /** 
-     * default constructor establishes 
+    /**
+     * default constructor establishes
      */
     public QueueNode() {
         this.tidVector = new Vector<Integer>();
     }
 
-    
-    /** 
+    /**
+     * Puts the process in this queue to sleep.
+     * 
      * @return int : method to put a thread to sleep in this queueNode
      */
     public synchronized int sleep() {
@@ -23,15 +23,19 @@ class QueueNode {
                 // do nothing
             }
         }
+        // remove the process at the head of the queue
         return this.tidVector.remove(0);
     }
 
-    
-    /** 
+    /**
+     * wakeup a thread belonging to this QueueNodes pid (condition), if the pid had
+     * more than one tid, any of the child threads belonging to this pid might be
+     * notified to execute
+     * 
      * @param tid : tid to add to this vector and potentially notify
      */
     public synchronized void wakeup(int tid) {
         this.tidVector.add(tid);
-        this.notify();
-    }    
+        this.notifyAll();
+    }
 }
